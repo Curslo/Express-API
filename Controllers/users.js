37e54262f2a99f675639b015b9d7804ddaa320ae
Curslo <richardkisivii@gmail.com>
2023-03-01 16:36:44 +0300

    Added user controller

---
+++ Controllers/users.js
@@ -0,0 +1,23 @@
+export const create = (user) => {
+    console.log('User')
+    return user;
+}
+
+export const update = (userId) => {
+    console.log('User')
+    return user;
+}
+
+export const remove = (userId) => {
+    console.log('User has been deleted')
+    return user;
+}
+
+export const findById = (userId) => {
+    console.log('User')
+    return user;
+}
+export const findAll = (user) => {
+    console.log('Users')
+    return user;
+}
